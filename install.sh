46353f467a150083bc82e425572f32c97cc711a8
Christopher Best <bestchris@gmail.com>
2013-07-15 21:04:43 -0400

    adding everything that was in the directory

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#cp "Soar (Raw).tmLanguage" ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
+cp "Soar (Raw).JSON-tmLanguage" ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
